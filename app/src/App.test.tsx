6535a7d97d7a5640cf0a1c49f33de1a0f6fb4d8e
AlphaBril <florian.marie.doucet@gmail.com>
2022-08-30 20:40:11 +0200

    remove wrong setting

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -5,9 +5,6 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store/configure';
 import { BrowserRouter } from 'react-router-dom';
-const { createServer } = require("http");
-const { Server } = require("socket.io");
-const Client = require("socket.io-client");
 
 window.matchMedia = window.matchMedia || function() {
   return {
@@ -16,63 +13,22 @@ window.matchMedia = window.matchMedia || function() {
       removeListener: function() {}
   };
 };
-
-describe("red tetris with pelican sauce", () => {
-  let io: any, serverSocket: any, clientSocket: any;
-
-  beforeAll((done) => {
-    const httpServer = createServer();
-    io = new Server(httpServer);
-    httpServer.listen(() => {
-      const port = httpServer.address().port;
-      clientSocket = new Client(`http://localhost:${port}`);
-      io.on("connection", (socket: any) => {
-        serverSocket = socket;
-      });
-      clientSocket.on("connect", done);
-    });
-  });
-
-  afterAll(() => {
-    io.close();
-    clientSocket.close();
-  });
-
-  // test("should work", (done) => {
-  //   clientSocket.on("hello", (arg: any) => {
-  //     expect(arg).toBe("world");
-  //     done();
-  //   });
-  //   serverSocket.emit("hello", "world");
-  // });
-
-  // test("should work (with ack)", (done) => {
-  //   serverSocket.on("hi", (cb: any) => {
-  //     cb("hola");
-  //   });
-  //   clientSocket.emit("hi", (arg: any) => {
-  //     expect(arg).toBe("hola");
-  //     done();
-  //   });
-  // });
-
-  test('render app', () => {
-    global.window = Object.create(window);
-  const url = "http://localhost:3000/#roomid";
-  Object.defineProperty(window, 'location', {
-    value: {
-      href: url
-    }
-  });
-    render(
-      <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-      </Provider>
-    );
-    const nameApp = screen.getByText(/RED-TETRIS/i);
-    expect(nameApp).toBeInTheDocument();
-  });
+test('render app', () => {
+  global.window = Object.create(window);
+const url = "http://localhost:3000/#roomid";
+Object.defineProperty(window, 'location', {
+  value: {
+    href: url
+  }
+});
+  render(
+    <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    </Provider>
+  );
+  const nameApp = screen.getByText(/RED-TETRIS/i);
+  expect(nameApp).toBeInTheDocument();
 });
 
